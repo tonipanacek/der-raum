6d01959202162f77efe6510289b18029ba660b8c
Alexander Dubovoy <ajdubovoy@gmail.com>
2020-01-17 11:38:20 +0100

    load projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,3 +1,26 @@
 <template>
   <div id="projects" />
 </template>
+
+<script>
+import { get, min, sortBy } from "lodash"
+
+export default {
+  async asyncData() {
+    // create context via webpack to map over all blog projects
+    const allProjects = await require.context(
+      "~/content/projects/",
+      true,
+      /\.md$/
+    )
+    let projects = allProjects.keys().map(key => {
+      // give back the value of each project context
+      return allProjects(key)
+    })
+    projects = sortBy(projects, project => project.position)
+    return {
+      projects
+    }
+  }
+}
+</script>
