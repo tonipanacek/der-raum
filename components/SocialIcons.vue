74f6bef3791fab132c1f4b159ce8578b07ce3a73
Toni Panacek <toni.panacek@gmail.com>
2020-01-28 19:30:23 +0100

    made social icons great again

--- components/SocialIcons.vue
+++ components/SocialIcons.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="social-icons">
-    <a href="http://www.instagram.com/der.raum" target="_blank">
-      <img svg-inline src="~/assets/images/linkedin.svg" alt="Linkedin Icon" class="social-item" />
-    </a>
-    <a href="https://www.pinterest.com/derraum" target="_blank" >
-      <img svg-inline src="~/assets/images/pinterest.svg" alt="Pinterest Icon" id="pinterest" class="social-item" />
-    </a>
-    <a href="https://www.facebook.com/derraumberlin" target="_blank">
-      <img svg-inline src="~/assets/images/twitter.svg" alt="Twitter Icon" class="social-item" />
-    </a>
-  </div>
+  <Cluster>
+    <div class="social-icons">
+      <a href="http://www.instagram.com/der.raum" target="_blank">
+        <img svg-inline src="~/assets/images/linkedin.svg" alt="Linkedin Icon" class="social-item" />
+      </a>
+      <a href="https://www.pinterest.com/derraum" target="_blank" >
+        <img svg-inline src="~/assets/images/pinterest.svg" alt="Pinterest Icon" id="pinterest" class="social-item" />
+      </a>
+      <a href="https://www.facebook.com/derraumberlin" target="_blank">
+        <img svg-inline src="~/assets/images/twitter.svg" alt="Twitter Icon" class="social-item" />
+      </a>
+    </div>
+  </Cluster>
 </template>
 
 <script>
+import Cluster from '~/components/Cluster'
 export default {
-  name: "SocialIcons"
+  name: "SocialIcons",
+  components: {
+    Cluster
+  }
 }
 </script>
 
@@ -28,8 +34,5 @@ svg#pinterest {
 }
 .social-icons {
   margin-top: spacing(sm);
-  display: flex;
-  width: 40ch;
-  justify-content: space-between;
 }
 </style>
