35112db435b5d02f4a4c48dc471dd349bf73ba97
Toni Panacek <toni.panacek@gmail.com>
2020-02-01 12:30:52 +0200

    legal and privacy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,51 +1,51 @@
 <template>
   <div id="privacy">
-    <h1>DATENSCHUTZ</h1>
-    <p>
-Wir freuen uns über Ihren Besuch auf unserer Website und Ihr Interesse an unseren Dienstleistungen. Der Schutz Ihrer personenbezogenen Daten ist für uns ein wichtiges Anliegen. Wir behandeln Ihre Daten vertraulich und entsprechend der gesetzlichen Vorschriften sowie dieser Datenschutzerklärung.
-
-Wir möchten, dass Sie sich beim Besuch unserer Internetseiten sicher fühlen. Im Folgenden erläutern wir, welche Informationen wir während Ihres Besuches auf unserer Website erfassen und wie diese genutzt werden.
-Datenschutzbeauftragte
-Für Auskünfte, Wünsche oder Anregungen zum Thema Datenschutz stehen wir Ihnen gerne zur Verfügung. Sie erreichen unseren betrieblichen Datenschutzbeauftragten unter der E-Mail: datenschutz(at)holz-raum.de
-Die für Datenschutz und Datensicherheit zuständige Aufsichtsbehörde erreichen Sie unter:
-Berliner Beauftragte für Datenschutz und Informationsfreiheit
-An der Urania 4-10
-10787 Berlin
-Nutzung von Google Analytics
-Diese Website nutzt Funktionen des Webanalysedienstes Google Analytics. Anbieter ist die Google Inc. 1600 Amphitheatre Parkway Mountain View, CA 94043, USA. Google Analytics verwendet sogenannte "Cookies". Das sind Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert.
-
-Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt.
-Sie können die Erfassung Ihrer Daten durch Google Analytics verhindern, indem Sie auf den folgenden Link klicken. Es wird ein sog. Opt-Out-Cookie gesetzt, durch den die zukünftige Erfassung Ihrer Daten beim Besuch dieses Internetauftritts ausgeschlossen ist: Google Analytics deaktivieren
-Informationen zum Datenschutzbestimmungen zu Google Analytics finden Sie unter http://www.google.com/intl/de/analytics/privacyoverview.html.
-Auf dieser Website wurde Google Analytics um den Code „anonymizeIp“ erweitert, um eine anonymisierte Erfassung von IP-Adressen (sog. IP-Masking) zu gewährleisten.
-Facebook-Plugins (Like-Button)
-Auf unseren Seiten sind Plugins des sozialen Netzwerks Facebook, Anbieter Facebook Inc., 1 Hacker Way, Menlo Park, California 94025, USA, integriert. Die Facebook-Plugins erkennen Sie an dem Facebook-Logo oder dem „Like-Button“ („Gefällt mir“) auf unserer Seite. Eine Übersicht über die Facebook-Plugins finden Sie hier: https://developers.facebook.com/docs/plugins/.
-
-Wenn Sie unsere Seiten besuchen, wird über das Plugin eine direkte Verbindung zwischen Ihrem Browser und dem Facebook-Server hergestellt. Facebook erhält dadurch die Information, dass Sie mit Ihrer IPAdresse unsere Seite besucht haben. Wenn Sie den Facebook „Like-Button“ anklicken während Sie in Ihrem Facebook-Account eingeloggt sind, können Sie die Inhalte unserer Seiten auf Ihrem Facebook-Profil verlinken. Dadurch kann Facebook den Besuch unserer Seiten Ihrem Benutzerkonto zuordnen. Wir weisen darauf hin, dass wir als Anbieter der Seiten keine Kenntnis vom Inhalt der übermittelten Daten sowie deren Nutzung durch Facebook erhalten. Weitere Informationen hierzu finden Sie in der Datenschutzerklärung von Facebook unter https://de-de.facebook.com/policy.php.
-
-Wenn Sie nicht wünschen, dass Facebook den Besuch unserer Seiten Ihrem Facebook-Nutzerkonto zuordnen kann, loggen Sie sich bitte aus Ihrem Facebook-Benutzerkonto aus.
-Server-Log-Files
-Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log Files, die Ihr Browser automatisch an uns übermittelt. Dies sind:
-Browsertyp/ Browserversion
-verwendetes Betriebssystem
-Referrer URL
-Hostname des zugreifenden Rechners
-Uhrzeit der Serveranfrage
-Diese Daten sind nicht bestimmten Personen zuordenbar. Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachträglich zu prüfen, wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden.
-Cookies
-Die Internetseiten verwenden Cookies. Cookies richten auf Ihrem Rechner keinen Schaden an und enthalten keine Viren. Sie dienen dazu, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. Die meisten der von uns verwendeten Cookies sind so genannte „Session-Cookies“. Sie werden nach Ende Ihres Besuchs automatisch gelöscht. Andere Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Diese Cookies ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.
-
-Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden und Cookies nur im Einzelfall erlauben, die Annahme von Cookies für bestimmte Fälle oder generell ausschließen sowie das automatische Löschen der Cookies beim Schließen des Browser aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität dieser Website eingeschränkt sein.
-Kontaktformular
-Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre Angaben zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
-Auskunftsrecht
-Als Nutzende unseres Internetangebotes haben Sie das Recht, von uns Auskunft über die zu Ihrer Person oder zu Ihrem Pseudonym gespeicherten Daten zu verlangen. Alle Angaben zur Datenverarbeitung sind in unserem Verfahrensverzeichnis ersichtlich, das Sie bei uns anfordern können. Auf Ihr Verlangen hin kann die Auskunft auch elektronisch erteilt werden.
-Sicherheit
-Wir setzen technische und organisatorische Sicherheitsmaßnahmen ein, um Ihre durch uns verwalteten personenbezogenen Daten gegen zufällige oder vorsätzliche Manipulationen, Verlust, Zerstörung oder gegen den Zugriff unberechtigter Personen zu schützen. Wir verbessern unsere Sicherheitsmaßnahmen entsprechend der technologischen Entwicklung fortlaufend.
-SSL-Verschlüsselung
-Diese Seite nutzt aus Gründen der Sicherheit und zum Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel der Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von „//“ auf „https://“ wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
-Wenn die SSL Verschlüsselung aktiviert ist, können die Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen werden.
-
-    </p>
+    <h1 class="title color:dark">DATENSCHUTZ</h1>
+    <p>Wir freuen uns über Ihren Besuch auf unserer Website und Ihr Interesse an unseren Dienstleistungen. Der Schutz Ihrer personenbezogenen Daten ist für uns ein wichtiges Anliegen. Wir behandeln Ihre Daten vertraulich und entsprechend der gesetzlichen Vorschriften sowie dieser Datenschutzerklärung.</p>
+    <p>Wir möchten, dass Sie sich beim Besuch unserer Internetseiten sicher fühlen. Im Folgenden erläutern wir, welche Informationen wir während Ihres Besuches auf unserer Website erfassen und wie diese genutzt werden.</p>
+    <h2 class="title">Datenschutzbeauftragte</h2>
+    <p>Für Auskünfte, Wünsche oder Anregungen zum Thema Datenschutz stehen wir Ihnen gerne zur Verfügung. Sie erreichen unseren betrieblichen Datenschutzbeauftragten unter der E-Mail: datenschutz(at)holz-raum.de
+    <br>Die für Datenschutz und Datensicherheit zuständige Aufsichtsbehörde erreichen Sie unter:
+    <br>Berliner Beauftragte für Datenschutz und Informationsfreiheit
+    <br>An der Urania 4-10
+    <br>10787 Berlin</p>
+    <p class="title">Nutzung von Google Analytics</p>
+    <p>Diese Website nutzt Funktionen des Webanalysedienstes Google Analytics. Anbieter ist die Google Inc. 1600 Amphitheatre Parkway Mountain View, CA 94043, USA. Google Analytics verwendet sogenannte "Cookies". Das sind Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert.</p>
+    <p>Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt.</p>
+    <p>Sie können die Erfassung Ihrer Daten durch Google Analytics verhindern, indem Sie auf den folgenden Link klicken. Es wird ein sog. Opt-Out-Cookie gesetzt, durch den die zukünftige Erfassung Ihrer Daten beim Besuch dieses Internetauftritts ausgeschlossen ist: Google Analytics deaktivieren
+    Informationen zum Datenschutzbestimmungen zu Google Analytics finden Sie unter http://www.google.com/intl/de/analytics/privacyoverview.html.</p>
+    <p>Auf dieser Website wurde Google Analytics um den Code „anonymizeIp“ erweitert, um eine anonymisierte Erfassung von IP-Adressen (sog. IP-Masking) zu gewährleisten.</p>
+    <p class="title">Facebook-Plugins (Like-Button)</p>
+    <p>Auf unseren Seiten sind Plugins des sozialen Netzwerks Facebook, Anbieter Facebook Inc., 1 Hacker Way, Menlo Park, California 94025, USA, integriert. Die Facebook-Plugins erkennen Sie an dem Facebook-Logo oder dem „Like-Button“ („Gefällt mir“) auf unserer Seite. Eine Übersicht über die Facebook-Plugins finden Sie hier: https://developers.facebook.com/docs/plugins/.</p>
+    <p>Wenn Sie unsere Seiten besuchen, wird über das Plugin eine direkte Verbindung zwischen Ihrem Browser und dem Facebook-Server hergestellt. Facebook erhält dadurch die Information, dass Sie mit Ihrer IPAdresse unsere Seite besucht haben. Wenn Sie den Facebook „Like-Button“ anklicken während Sie in Ihrem Facebook-Account eingeloggt sind, können Sie die Inhalte unserer Seiten auf Ihrem Facebook-Profil verlinken. Dadurch kann Facebook den Besuch unserer Seiten Ihrem Benutzerkonto zuordnen. Wir weisen darauf hin, dass wir als Anbieter der Seiten keine Kenntnis vom Inhalt der übermittelten Daten sowie deren Nutzung durch Facebook erhalten. Weitere Informationen hierzu finden Sie in der Datenschutzerklärung von Facebook unter https://de-de.facebook.com/policy.php.</p>
+    <p>Wenn Sie nicht wünschen, dass Facebook den Besuch unserer Seiten Ihrem Facebook-Nutzerkonto zuordnen kann, loggen Sie sich bitte aus Ihrem Facebook-Benutzerkonto aus.</p>
+    <p class="title">Server-Log-Files</p>
+    <p>Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log Files, die Ihr Browser automatisch an uns übermittelt. Dies sind:
+    <br>Browsertyp/ Browserversion
+    <br>verwendetes Betriebssystem
+    <br>Referrer URL
+    <br>Hostname des zugreifenden Rechners
+    <br>Uhrzeit der Serveranfrage
+    <br>Diese Daten sind nicht bestimmten Personen zuordenbar. Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachträglich zu prüfen, wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden.
+    <p class="title">Cookies</p>
+    <p>Die Internetseiten verwenden Cookies. Cookies richten auf Ihrem Rechner keinen Schaden an und enthalten keine Viren. Sie dienen dazu, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. Die meisten der von uns verwendeten Cookies sind so genannte „Session-Cookies“. Sie werden nach Ende Ihres Besuchs automatisch gelöscht. Andere Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Diese Cookies ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.</p>
+    <p>Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden und Cookies nur im Einzelfall erlauben, die Annahme von Cookies für bestimmte Fälle oder generell ausschließen sowie das automatische Löschen der Cookies beim Schließen des Browser aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität dieser Website eingeschränkt sein.
+    <p class="title">Kontaktformular</p>
+    <p>Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre Angaben zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+    <br>Auskunftsrecht
+    <br>Als Nutzende unseres Internetangebotes haben Sie das Recht, von uns Auskunft über die zu Ihrer Person oder zu Ihrem Pseudonym gespeicherten Daten zu verlangen. Alle Angaben zur Datenverarbeitung sind in unserem Verfahrensverzeichnis ersichtlich, das Sie bei uns anfordern können. Auf Ihr Verlangen hin kann die Auskunft auch elektronisch erteilt werden.
+    <p class="title">Sicherheit</p>
+    <p>Wir setzen technische und organisatorische Sicherheitsmaßnahmen ein, um Ihre durch uns verwalteten personenbezogenen Daten gegen zufällige oder vorsätzliche Manipulationen, Verlust, Zerstörung oder gegen den Zugriff unberechtigter Personen zu schützen. Wir verbessern unsere Sicherheitsmaßnahmen entsprechend der technologischen Entwicklung fortlaufend.
+    <p class="title">SSL-Verschlüsselung</p>
+    <p>Diese Seite nutzt aus Gründen der Sicherheit und zum Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel der Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von „//“ auf „https://“ wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
+    Wenn die SSL Verschlüsselung aktiviert ist, können die Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen werden.</p>
   </div>
 </template>
+<style lang="scss">
+  #privacy {
+    h1 {
+      margin-top: 0;
+      font-size: 1rem;
+    }
+  }
+</style>
