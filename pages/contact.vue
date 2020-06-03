398269c9268ede25a36c8fee0371cdddd26c4be8
Toni Panacek <toni.panacek@gmail.com>
2020-06-03 12:20:07 +0200

    text aligned on this crazy contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,37 +3,37 @@
     <Article class="contact">
       <div class="image-container">
         <Frame :source="$t('contact.cloudinaryImageUrl')">
-          <!-- <img :src="$t('contact.cloudinaryImageUrl')"> -->
         </Frame>
       </div>
         <div class="text">
           <div class="info">
-            <h1>{{ $t("contact.thirdColumn") }}</h1>
-            <a href="mailto:derraum@holz-raum.de" class="contact-link email">
+            <h1>{{ $t("contact.firstColumn") }}</h1>
+            <a href="mailto:derraum@holz-raum.de" class=" contact-link email">
               {{ $t("contact.email") }}
             </a>
             <div class="justified">
               <a href="tel:017673564742" class="contact-link tel" id="tel1">
-                {{ $t("contact.tel2") }}
+                {{ $t("contact.telephone2") }}
               </a><span class="white-space"></span>
               <a href="tel:03039375300" class="contact-link tel" id="tel2">
-                {{ $t("contact.tel") }}
+                {{ $t("contact.telephone1") }}
               </a>
             </div>
           </div>
           <div class="info">
-            <h2>{{ $t("contact.firstColumn") }}</h2>
+            <h2>{{ $t("contact.secondColumn") }}</h2>
             <a href="https://www.google.com/maps/search/?api=1&query=der%20raum" target="_blank" class="contact-link address">
               {{ $t("contact.streetNumber") }}<span class="white-space"></span>{{ $t("contact.cityZip") }}
             </a>
             <div class="justified hours">
-              <p class=" contact-link">{{ $t("contact.weekdays") }}<span class="mobile1"></span> {{ $t("contact.weekdayHours") }}</p><span class="white-space"></span>
-              <p class=" contact-link">{{ $t("contact.weekend") }}<span class="mobile2"></span> {{ $t("contact.weekendHours") }}</p>
+              <p class="no-margin">{{ $t("contact.weekdays") }}<span class="mobile1"></span> {{ $t("contact.weekdayHours") }}</p><span class="white-space"></span>
+              <p class="no-margin">{{ $t("contact.weekend") }}<span class="mobile2"></span> {{ $t("contact.weekendHours") }}</p>
             </div>
           </div>
           <div class="info">
-            <h3>Application</h3>
-            <p class=" contact-link">{{ $t("contact.cvShort") }}</p>
+            <h3>{{ $t("contact.thirdColumn") }}</h3>
+            <p class="no-margin">{{ $t("contact.cvLine1") }}</p>
+            <p class="no-margin">{{ $t("contact.cvLine2") }}</p>
           </div>
         </div>
     </Article>
@@ -74,17 +74,19 @@ export default {
     flex-flow: row wrap;
     width: 100%;
     align-items: flex-start;
-    .info:last-child {
-      width: 220px;
-    }
-    .info:not(:last-child) {
-      margin-right: 5%;
+    .info{
       margin-bottom: 10%;
       @include respond-to('large') {
         margin-bottom: 3%;
       }
-      p { margin-top: 0; }
+      &:not(:last-child) {
+        margin-right: 5%;
+      }
+      &:last-child {
+        width: 220px;
+      }
       a { font-size: .8em; }
+      h1, h2, h3 { margin: .75rem 0; }
       .email, .address { display: block; }
       .justified {
         display: flex;
@@ -104,14 +106,18 @@ export default {
         text-decoration: none;
         color: color(dark);
         transition: font-weight 100ms ease;
-        line-height: 2em;
-        margin-bottom: .5em;
+        line-height: 2rem;
+        margin-bottom: .5rem;
         &:hover {
           a{
             color: color(dark);
             font-weight: 600;
           }
         }
+      }
+      .no-margin {
+        margin-top: 0;
+        margin-bottom: .5rem;
       }
       .hours {
         display: flex;
