24741bfa962b113cc4fa00fa4389d43eef815824
Toni Panacek <toni.panacek@gmail.com>
2020-08-11 17:09:00 +0200

    innerHeight

--- pages/projects/_slug/images/_id.vue
+++ pages/projects/_slug/images/_id.vue
@@ -159,9 +159,7 @@ export default {
         mainContainer.classList.add('mt-lg')
       }
       const setDocHeight = () => {
-        // document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
-        document.documentElement.style.height = '100%';
-        document.documentElement.style.overflow = 'hidden';
+        document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
       }
       const widthChange = (mq) => {
         if ((this.$route.path.includes('bilder') || this.$route.path.includes('images')) && mq.matches) {
