37bf8fbb89fa503222070c4169138a8387c30f3b
Toni Panacek <toni.panacek@gmail.com>
2021-02-05 12:42:12 +0100

    added container and article but text still being weird

--- components/TextImageGrid.vue
+++ components/TextImageGrid.vue
@@ -1,29 +1,33 @@
 <template>
   <Container class="text-image-grid" :id="formatSlug($ta(page.attributes, 'title'))">
-    <template v-for="(section, index) in sections">
-      <div v-if="section.image" class="section-item image-container">
-        <div class="frame-wrapper">
-          <Frame :n="findOrientation(index) === 'portrait' ? 9 : 12" :d="findOrientation(index) === 'portrait' ? 16 : 9">
-            <img :src="$ta(section, 'image')"/>
-          </Frame>
+      <template v-for="(section, index) in sections">
+        <div v-if="section.image" class="section-item image-container">
+          <div class="frame-wrapper">
+            <Frame :n="findOrientation(index) === 'portrait' ? 9 : 12" :d="findOrientation(index) === 'portrait' ? 16 : 9">
+              <img :src="$ta(section, 'image')"/>
+            </Frame>
+          </div>
         </div>
-      </div>
-      <div v-else class="section-item text">
-        <h1 v-if="section.de_title_of_section">{{ $ta(section, "title_of_section") }}</h1>
-        <vue-markdown>{{ $ta(section, "text_section") }}</vue-markdown>
-      </div>
-    </template>
+        <div v-else class="section-item text">
+          <h1 v-if="section.de_title_of_section">{{ $ta(section, "title_of_section") }}</h1>
+          <vue-markdown>{{ $ta(section, "text_section") }}</vue-markdown>
+        </div>
+      </template>
   </Container>
 </template>
 
 <script>
 import { get } from 'lodash'
 import Frame from '~/components/Frame'
+import Container from '~/components/Container'
+import Article from '~/components/Article'
 
 export default {
   name: "TextImageGrid",
   components: {
-    Frame
+    Frame,
+    Container,
+    Article
   },
   props: {
     page: {
@@ -46,7 +50,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .text-image-grid {
     @include respond-to('large') {
     .image-container {
